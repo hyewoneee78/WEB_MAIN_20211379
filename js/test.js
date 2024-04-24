87bd4e5fc38ab26a4c0d65bdb8bda87ffd1fc48b
admin <whyewon@naver.com>
2024-04-24 16:31:18 +0900

    7주차

--- js/test.js
+++ js/test.js
@@ -14,8 +14,3 @@ console.log(d);
 
 const e = "상수1 접근";
 console.log(e);
-
-document.getElementById("search_btn").addEventListener("click", search_message);
-function search_message() {
-  alert("검색을 수행합니다!");
-}
